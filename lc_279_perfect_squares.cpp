90f5923beb00275d5794aa0ca66e14f8fc400699
Hejian <hjsang@iastate.edu>
2019-09-19 16:45:54 -0400

    perfect squares

---
+++ lc_279_perfect_squares.cpp
@@ -0,0 +1,59 @@
+/* 279. Perfect Squares
+Given a positive integer n,
+find the least number of perfect square numbers 
+(for example, 1, 4, 9, 16, ...) which sum to n.
+
+Example 1:
+
+Input: n = 12
+Output: 3 
+Explanation: 12 = 4 + 4 + 4.
+
+
+Example 2:
+
+Input: n = 13
+Output: 2
+Explanation: 13 = 4 + 9.
+
+*/
+
+/* Solution:
+All possible results are {1,2,3,4}.
+if n%4==0, n= n/4;
+if (n % 8 == 7) return 4;
+if n = a^2+b^2 return length(unique(a,b))
+else return 3.
+*/
+class Solution {
+public:
+    int numSquares(int n) {
+        while (n % 4 == 0) n /= 4;
+        if (n % 8 == 7) return 4;
+        for (int a = 0; a * a <= n; ++a) {
+            int b = sqrt(n - a * a);
+            if (a * a + b * b == n) {
+                return !!a + !!b;
+            }
+        }
+        return 3;
+    }
+};
+
+/* Solution: DP
+
+*/
+
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> dp(n + 1, INT_MAX);
+        dp[0] = 0;
+        for (int i = 0; i <= n; ++i) {
+            for (int j = 1; i + j * j <= n; ++j) {
+                dp[i + j * j] = min(dp[i + j * j], dp[i] + 1);
+            }
+        }
+        return dp.back();
+    }
+};
