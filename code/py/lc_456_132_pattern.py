025813d8469a44f9631713fc8b86dc18ab4f335f
HJSang <sanghj0923@gmail.com>
2020-05-31 16:32:02 -0400

    add comments to lc 456

--- code/py/lc_456_132_pattern.py
+++ code/py/lc_456_132_pattern.py
@@ -7,15 +7,16 @@
 
 # Idea: The number which larger than the **third** and before **third** is stored in the stack.
 
-```python
 class Solution:
   def find132pattern(self, nums: List[int]) -> bool:
     stack, s3 = [], -float('Inf')
     for n in nums[::-1]:
       if n<s3:
+        # return True because n < s3 < stack[-1] (132 pattern)
         return True
       while stack and stack[-1] < n:
+        # if stack is not empty, pop the last element if <n
         s3=stack.pop()
+      # while loop stops when stack is empty or stack[-1]>=n  
       stack.append(n)
     return False    
-```
