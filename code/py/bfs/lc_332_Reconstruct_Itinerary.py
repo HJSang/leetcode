4423a9b151139c1f24e7c709cada1a30ef660411
HJSang <sanghj0923@gmail.com>
2020-06-09 11:12:10 -0400

    lc 332

---
+++ code/py/bfs/lc_332_Reconstruct_Itinerary.py
@@ -0,0 +1,67 @@
+# 332. Reconstruct Itinerary
+# Given a list of airline tickets represented by pairs of departure and arrival airports [from, to]
+# reconstruct the itinerary in order. All of the tickets belong to a man who departs from JFK
+# Thus, the itinerary must begin with JFK.
+# Note:
+# If there are multiple valid itineraries,  you should return the itinerary that has the smallest
+# lexical order when read as a single string. For example, the itinerary ["JFK", "LGA"] has a
+# smaller lexical order than ["JFK", "LGB"]
+
+# All airports are represented by three capital letters (IATA code)
+# You may assume all tickets form at least one valid itinerary.
+
+# Example 1:
+# Input: [["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]
+# Output: ["JFK", "MUC", "LHR", "SFO", "SJC"]
+
+# Example 2:
+# Input: [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+# Output: ["JFK","ATL","JFK","SFO","ATL","SFO"]
+# Explanation: Another possible reconstruction is ["JFK","SFO","ATL","JFK","ATL","SFO"].
+# But it is larger in lexical order.
+
+class Solution(object):
+    """  """
+    def findItinerary(self, tickets):
+        """
+        :type tickets: List[List[str]]
+        :rtype: List[str]
+        """
+        from collections import defaultdict
+        self.flightMap = defaultdict(list)
+
+        for ticket in tickets:
+            origin, dest = ticket[0], ticket[1]
+            self.flightMap[origin].append(dest)
+
+        self.visitBitmap = {}
+
+        # sort the itinerary based on the lexical order
+        for origin, itinerary in self.flightMap.items():
+        # Note that we could have multiple identical flights, i.e. same origin and destination.
+            itinerary.sort()
+            self.visitBitmap[origin] = [False]*len(itinerary)
+
+        self.flights = len(tickets)
+        self.result = []
+        route = ['JFK']
+        self.backtracking('JFK', route)
+
+        return self.result
+
+
+    def backtracking(self, origin, route):
+        if len(route) == self.flights + 1:
+            self.result = route
+            return True
+
+        for i, nextDest in enumerate(self.flightMap[origin]):
+            if not self.visitBitmap[origin][i]:
+                # mark the visit before the next recursion
+                self.visitBitmap[origin][i] = True
+                ret = self.backtracking(nextDest, route + [nextDest])
+                self.visitBitmap[origin][i] = False
+                if ret:
+                    return True
+
+        return False
