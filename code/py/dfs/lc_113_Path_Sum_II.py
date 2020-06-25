b1dc9267074bfe221595b54bf114ebe4ccfbdb79
HJSang <sanghj0923@gmail.com>
2020-06-25 10:24:11 -0400

    lc 133 iterative solution

--- code/py/dfs/lc_113_Path_Sum_II.py
+++ code/py/dfs/lc_113_Path_Sum_II.py
@@ -47,3 +47,36 @@ class Solution:
     if root.right:
       self.dfs(root.right, sum-root.val, ls+[root.val], res)
 
+# BFS using stack 
+class Solution:
+  def pathSum(self, root: TreeNode, sum: int) -> List[List[int]]:
+    if not root:
+      return []
+    res = []
+    stack = [(root, root.val, [root.val])]
+    while stack:
+      cur, val, ls = stack.pop()
+      if not cur.left and not cur.right and val == sum:
+        res.append(ls)
+      if cur.right:
+        stack.append((cur.right, val+cur.right.val, ls+[cur.right.val]))
+      if cur.left:
+        stack.append((cur.left, val+cur.left.val, ls+[cur.left.val]))
+    return res
+
+class Solution:
+    def pathSum(self, root: TreeNode, sum: int) -> List[List[int]]:
+        if not root:
+            return []
+        res = []
+        stack = [(root, sum-root.val, [root.val])]
+        while stack:
+            curr, val, ls = stack.pop()
+            if not curr.left and not curr.right and val == 0:
+                res.append(ls)
+            if curr.right:
+                stack.append((curr.right, val-curr.right.val, ls+[curr.right.val]))
+            if curr.left:
+                stack.append((curr.left, val-curr.left.val, ls+[curr.left.val]))
+        return res 
+
