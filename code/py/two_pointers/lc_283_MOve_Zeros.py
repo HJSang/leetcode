e7c138c61bc9f92cd53d98dcf0a9c9fffe433380
HJSang <sanghj0923@gmail.com>
2020-06-28 21:22:42 -0400

    lc 283

---
+++ code/py/two_pointers/lc_283_MOve_Zeros.py
@@ -0,0 +1,23 @@
+# 283. Move Zeroes
+
+# Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+# 
+# Example:
+# 
+# Input: [0,1,0,3,12]
+# Output: [1,3,12,0,0]
+# Note:
+# 
+# You must do this in-place without making a copy of the array.
+# Minimize the total number of operations.
+
+class Solution:
+  def moveZeros(self, nums: List[int]) -> None:
+    # save the index of zero position
+    zero = 0
+    for i in range(len(nums)):
+      if nums[i] != 0:
+        # swap with 0 
+        nums[i], nums[zero] = nums[zero], nums[i]
+        zero +=1
+
