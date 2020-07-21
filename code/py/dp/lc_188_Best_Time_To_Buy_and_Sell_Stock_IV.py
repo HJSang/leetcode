7e91ce1ab7c729e5f949c2127bdf84f06d2558ec
HJSang <sanghj0923@gmail.com>
2020-07-21 09:24:07 -0400

    lc 188

---
+++ code/py/dp/lc_188_Best_Time_To_Buy_and_Sell_Stock_IV.py
@@ -0,0 +1,43 @@
+# 188. Best Time to Buy and Sell Stock IV
+
+# Hard
+
+# Say you have an array for which the i-th element is the price of a given stock on day i.
+# 
+# Design an algorithm to find the maximum profit. You may complete at most k transactions.
+# 
+# Note:
+# You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+# 
+# Example 1:
+# 
+# Input: [2,4,1], k = 2
+# Output: 2
+# Explanation: Buy on day 1 (price = 2) and sell on day 2 (price = 4), profit = 4-2 = 2.
+# Example 2:
+# 
+# Input: [3,2,6,5,0,3], k = 2
+# Output: 7
+# Explanation: Buy on day 2 (price = 2) and sell on day 3 (price = 6), profit = 6-2 = 4.
+#              Then buy on day 5 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        n = len(prices)
+        # max n//2 buy and sell
+        if k>=n//2:
+            profit = 0
+            for i in range(1,n):
+                profit += max(0, prices[i]-prices[i-1])
+            return profit
+        # 2d-dp
+        # dp[i][j]: max profit for [0,j] with at most i trades
+        # dp[i][j] = d[i][j-1] if no action on j
+        # dp[i][j] = max(dp[i-1][t-1] + prices[j]-prices[t]) w.r.t t if selling on j and but on t
+        dp=[[0]*n for _ in range(k+1)]
+        for i in range(1,k+1):
+            maxtemp = -prices[0]
+            for j in range(1,n):
+                dp[i][j] = max(dp[i][j-1], prices[j]+maxtemp)
+                maxtemp = max(maxtemp, dp[i-1][j-1]-prices[j])
+        return dp[k][n-1]  
